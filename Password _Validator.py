cf5b83f3783a3cf6d027c656c8fe5eda98061842
Meet Tailor <meett2600@gmail.com>
2026-01-15 21:26:15 +0530

    Add files via upload

---
+++ Password _Validator.py
@@ -0,0 +1,18 @@
+pwd = input("Enter Your Password: ")
+p = len(pwd)
+a = False
+b = False
+c = False
+
+for ch in pwd:
+    if ch.isupper():
+        a = True
+    if ch.islower():
+        b = True
+    if ch.isdigit():
+        c = True
+
+if 8 < p < 15 and a and b and c:
+    print("Password is valid")
+else:
+    print("Password is invalid! Need to Make More Strong Password")
